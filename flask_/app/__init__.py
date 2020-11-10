fe772350e18e0a2d03248aac5da491704cc66f77
govindkrvr <govindkrvr@gmail.com>
2020-11-10 09:42:03 +0530

    flask

--- flask_/app/__init__.py
+++ flask_/app/__init__.py
@@ -6,10 +6,9 @@ import config
 application = Flask(__name__)
 
 application.config['SECRET_KEY'] = "random_string"
-#application.config['MONGOALCHEMY_SERVER'] = config.configDB['IP']
-#application.config['MONGOALCHEMY_PORT'] = config.configDB['PORT']
+application.config['MONGOALCHEMY_SERVER'] = config.configDB['IP']
+application.config['MONGOALCHEMY_PORT'] = config.configDB['PORT']
 application.config['MONGOALCHEMY_DATABASE'] = config.configDB['DATABASE']
-application.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://127.0.0.1:27017'
 db = MongoAlchemy(application)
 
 from app import views
